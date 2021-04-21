629dc8839912ddf6426c9540a2c0b1380f96f632
Richard Ramos <info@richardramos.me>
2021-04-20 20:22:12 -0400

    fix: incorrect name stored on settings

--- src/login/onboarding-model.cpp
+++ src/login/onboarding-model.cpp
@@ -35,18 +35,12 @@ GeneratedAccount jsonObjectToAccount(const QJsonObject obj)
 
 void setDerivedKeys(GeneratedAccount& acc, const QJsonObject obj)
 {
-	acc.derivedKeys = {{Constants::PathWalletRoot,
-						{obj[Constants::PathWalletRoot]["publicKey"].toString(),
-						 obj[Constants::PathWalletRoot]["address"].toString()}},
-					   {Constants::PathEip1581,
-						{obj[Constants::PathEip1581]["publicKey"].toString(),
-						 obj[Constants::PathEip1581]["address"].toString()}},
-					   {Constants::PathWhisper,
-						{obj[Constants::PathWhisper]["publicKey"].toString(),
-						 obj[Constants::PathWhisper]["address"].toString()}},
-					   {Constants::PathDefaultWallet,
-						{obj[Constants::PathDefaultWallet]["publicKey"].toString(),
-						 obj[Constants::PathDefaultWallet]["address"].toString()}}};
+	acc.derivedKeys = {
+		{Constants::PathWalletRoot, {obj[Constants::PathWalletRoot]["publicKey"].toString(), obj[Constants::PathWalletRoot]["address"].toString()}},
+		{Constants::PathEip1581, {obj[Constants::PathEip1581]["publicKey"].toString(), obj[Constants::PathEip1581]["address"].toString()}},
+		{Constants::PathWhisper, {obj[Constants::PathWhisper]["publicKey"].toString(), obj[Constants::PathWhisper]["address"].toString()}},
+		{Constants::PathDefaultWallet,
+		 {obj[Constants::PathDefaultWallet]["publicKey"].toString(), obj[Constants::PathDefaultWallet]["address"].toString()}}};
 }
 
 QVector<GeneratedAccount> multiAccountGenerateAndDeriveAddresses()
@@ -54,15 +48,10 @@ QVector<GeneratedAccount> multiAccountGenerateAndDeriveAddresses()
 	QJsonObject obj{{"n", 5},
 					{"mnemonicPhraseLength", 12},
 					{"bip32Passphrase", ""},
-					{"paths",
-					 QJsonArray{Constants::PathWalletRoot,
-								Constants::PathEip1581,
-								Constants::PathWhisper,
-								Constants::PathDefaultWallet}}
+					{"paths", QJsonArray{Constants::PathWalletRoot, Constants::PathEip1581, Constants::PathWhisper, Constants::PathDefaultWallet}}
 
 	};
-	const char* result = MultiAccountGenerateAndDeriveAddresses(
-		Utils::jsonToStr(obj).toUtf8().data()); // TODO: clear from memory
+	const char* result = MultiAccountGenerateAndDeriveAddresses(Utils::jsonToStr(obj).toUtf8().data()); // TODO: clear from memory
 	QJsonArray multiAccounts = QJsonDocument::fromJson(result).array();
 
 	QVector<GeneratedAccount> vector;
@@ -109,16 +98,10 @@ QVariant OnboardingModel::data(const QModelIndex& index, int role) const
 
 	switch(role)
 	{
-	case Id:
-		return QVariant(mData[index.row()].keyUid);
-	case PublicKey:
-		return QVariant(mData[index.row()].derivedKeys.at(Constants::PathWhisper).publicKey);
-	case Image:
-		return QVariant(Utils::generateIdenticon(
-			mData[index.row()].derivedKeys.at(Constants::PathWhisper).publicKey));
-	case Name:
-		return QVariant(Utils::generateAlias(
-			mData[index.row()].derivedKeys.at(Constants::PathWhisper).publicKey));
+	case Id: return QVariant(mData[index.row()].keyUid);
+	case PublicKey: return QVariant(mData[index.row()].derivedKeys.at(Constants::PathWhisper).publicKey);
+	case Image: return QVariant(Utils::generateIdenticon(mData[index.row()].derivedKeys.at(Constants::PathWhisper).publicKey));
+	case Name: return QVariant(Utils::generateAlias(mData[index.row()].derivedKeys.at(Constants::PathWhisper).publicKey));
 	}
 
 	return QVariant();
@@ -147,29 +130,22 @@ QVariantMap OnboardingModel::get(int row) const
 void storeDerivedAccount(QString& accountId, QString& hashedPassword)
 {
 	QJsonObject obj{{"accountID", accountId},
-					{"paths",
-					 QJsonArray{Constants::PathWalletRoot,
-								Constants::PathEip1581,
-								Constants::PathWhisper,
-								Constants::PathDefaultWallet}},
+					{"paths", QJsonArray{Constants::PathWalletRoot, Constants::PathEip1581, Constants::PathWhisper, Constants::PathDefaultWallet}},
 					{"password", hashedPassword}};
 
-	MultiAccountStoreDerivedAccounts(Utils::jsonToStr(obj).toUtf8().data());
+	const char* multiaccounstorederivedresult = MultiAccountStoreDerivedAccounts(Utils::jsonToStr(obj).toUtf8().data());
 
 	// TODO: clear obj
 }
 
 QJsonObject getAccountData(GeneratedAccount* account)
 {
-	return QJsonObject{
-		{"name", Utils::generateAlias(account->derivedKeys[Constants::PathWhisper].publicKey)},
-		{"address", account->address},
-		{"photo-path",
-		 Utils::generateIdenticon(account->derivedKeys[Constants::PathWhisper].publicKey)},
-		{"identicon",
-		 Utils::generateIdenticon(account->derivedKeys[Constants::PathWhisper].publicKey)},
-		{"key-uid", account->keyUid},
-		{"keycard-pairing", QJsonValue()}};
+	return QJsonObject{{"name", Utils::generateAlias(account->derivedKeys[Constants::PathWhisper].publicKey)},
+					   {"address", account->address},
+					   {"photo-path", Utils::generateIdenticon(account->derivedKeys[Constants::PathWhisper].publicKey)},
+					   {"identicon", Utils::generateIdenticon(account->derivedKeys[Constants::PathWhisper].publicKey)},
+					   {"key-uid", account->keyUid},
+					   {"keycard-pairing", QJsonValue()}};
 }
 
 QString generateSigningPhrase(int count)
@@ -177,8 +153,7 @@ QString generateSigningPhrase(int count)
 	QStringList words;
 	for(int i = 0; i < count; i++)
 	{
-		words.append(
-			phrases[QRandomGenerator::global()->bounded(static_cast<int>(phrases.size()))]);
+		words.append(phrases[QRandomGenerator::global()->bounded(static_cast<int>(phrases.size()))]);
 	}
 	return words.join(" ");
 }
@@ -191,31 +166,28 @@ QJsonObject getAccountSettings(GeneratedAccount* account, QString installationId
 	QString defaultNetworksContent = defaultNetworks.readAll();
 	defaultNetworksContent = defaultNetworksContent.replace("%INFURA_KEY%", INFURA_KEY);
 
-	QJsonArray defaultNetworksJson =
-		QJsonDocument::fromJson(defaultNetworksContent.toUtf8()).array();
+	QJsonArray defaultNetworksJson = QJsonDocument::fromJson(defaultNetworksContent.toUtf8()).array();
 
-	return QJsonObject{
-		{"key-uid", account->keyUid},
-		{"mnemonic", account->mnemonic},
-		{"public-key", account->derivedKeys[Constants::PathWhisper].publicKey},
-		{"name", Utils::generateAlias(account->publicKey)},
-		{"address", account->address},
-		{"eip1581-address", account->derivedKeys[Constants::PathEip1581].address},
-		{"dapps-address", account->derivedKeys[Constants::PathDefaultWallet].address},
-		{"wallet-root-address", account->derivedKeys[Constants::PathDefaultWallet].address},
-		{"preview-privacy?", true},
-		{"signing-phrase", generateSigningPhrase(3)},
-		{"log-level", "INFO"},
-		{"latest-derived-path", 0},
-		{"networks/networks", defaultNetworksJson},
-		{"currency", "usd"},
-		{"identicon",
-		 Utils::generateIdenticon(account->derivedKeys[Constants::PathWhisper].publicKey)},
-		{"waku-enabled", true},
-		{"wallet/visible-tokens", {{"mainnet", QJsonArray{"SNT"}}}},
-		{"appearance", 0},
-		{"networks/current-network", Constants::DefaultNetworkName},
-		{"installation-id", installationId}};
+	return QJsonObject{{"key-uid", account->keyUid},
+					   {"mnemonic", account->mnemonic},
+					   {"public-key", account->derivedKeys[Constants::PathWhisper].publicKey},
+					   {"name", Utils::generateAlias(account->derivedKeys[Constants::PathWhisper].publicKey)},
+					   {"address", account->address},
+					   {"eip1581-address", account->derivedKeys[Constants::PathEip1581].address},
+					   {"dapps-address", account->derivedKeys[Constants::PathDefaultWallet].address},
+					   {"wallet-root-address", account->derivedKeys[Constants::PathWalletRoot].address},
+					   {"preview-privacy?", true},
+					   {"signing-phrase", generateSigningPhrase(3)},
+					   {"log-level", "INFO"},
+					   {"latest-derived-path", 0},
+					   {"networks/networks", defaultNetworksJson},
+					   {"currency", "usd"},
+					   {"identicon", Utils::generateIdenticon(account->derivedKeys[Constants::PathWhisper].publicKey)},
+					   {"waku-enabled", true},
+					   {"wallet/visible-tokens", {{"mainnet", QJsonArray{"SNT"}}}},
+					   {"appearance", 0},
+					   {"networks/current-network", Constants::DefaultNetworkName},
+					   {"installation-id", installationId}};
 
 	// TODO: clear mnemonic?
 }
@@ -234,10 +206,7 @@ QJsonObject getNodeConfig(QString installationId)
 
 	QFile fleets(":/resources/fleets.json");
 	fleets.open(QIODevice::ReadOnly);
-	QJsonObject fleetJson = QJsonDocument::fromJson(fleets.readAll())
-								.object()["fleets"]
-								.toObject()["eth.prod"]
-								.toObject();
+	QJsonObject fleetJson = QJsonDocument::fromJson(fleets.readAll()).object()["fleets"].toObject()["eth.prod"].toObject();
 
 	QJsonObject clusterConfig = nodeConfigJson["ClusterConfig"].toObject();
 
@@ -272,27 +241,23 @@ QJsonObject getNodeConfig(QString installationId)
 
 QJsonArray getSubAccountData(GeneratedAccount* account)
 {
-	return QJsonArray{
-		QJsonObject{{"public-key", account->derivedKeys[Constants::PathDefaultWallet].publicKey},
-					{"address", account->derivedKeys[Constants::PathDefaultWallet].address},
-					{"color", "#4360df"},
-					{"wallet", true},
-					{"path", Constants::PathDefaultWallet},
-					{"name", "Status account"}},
-		QJsonObject{
-			{"public-key", account->derivedKeys[Constants::PathWhisper].publicKey},
-			{"address", account->derivedKeys[Constants::PathWhisper].address},
-			{"path", Constants::PathWhisper},
-			{"name", Utils::generateAlias(account->derivedKeys[Constants::PathWhisper].publicKey)},
-			{"identicon",
-			 Utils::generateIdenticon(account->derivedKeys[Constants::PathWhisper].publicKey)},
-			{"chat", true}}};
+	return QJsonArray{QJsonObject{{"public-key", account->derivedKeys[Constants::PathDefaultWallet].publicKey},
+								  {"address", account->derivedKeys[Constants::PathDefaultWallet].address},
+								  {"color", "#4360df"},
+								  {"wallet", true},
+								  {"path", Constants::PathDefaultWallet},
+								  {"name", "Status account"}},
+					  QJsonObject{{"public-key", account->derivedKeys[Constants::PathWhisper].publicKey},
+								  {"address", account->derivedKeys[Constants::PathWhisper].address},
+								  {"path", Constants::PathWhisper},
+								  {"name", Utils::generateAlias(account->derivedKeys[Constants::PathWhisper].publicKey)},
+								  {"identicon", Utils::generateIdenticon(account->derivedKeys[Constants::PathWhisper].publicKey)},
+								  {"chat", true}}};
 }
 
 bool saveAccountAndLogin(GeneratedAccount* genAccount, QString password)
 {
-	QString hashedPassword = QString::fromUtf8(
-		QCryptographicHash::hash(password.toUtf8(), QCryptographicHash::Keccak_256));
+	QString hashedPassword = "0x" + QString::fromUtf8(QCryptographicHash::hash(password.toUtf8(), QCryptographicHash::Keccak_256).toHex());
 
 	storeDerivedAccount(genAccount->id, hashedPassword);
 
@@ -302,11 +267,11 @@ bool saveAccountAndLogin(GeneratedAccount* genAccount, QString password)
 	QJsonObject nodeConfig(getNodeConfig(installationId));
 	QJsonArray subAccountData(getSubAccountData(genAccount));
 
-	qDebug() << SaveAccountAndLogin(Utils::jsonToStr(accountData).toUtf8().data(),
-									hashedPassword.toUtf8().data(),
-									Utils::jsonToStr(settings).toUtf8().data(),
-									Utils::jsonToStr(nodeConfig).toUtf8().data(),
-									Utils::jsonToStr(subAccountData).toUtf8().data());
+	SaveAccountAndLogin(Utils::jsonToStr(accountData).toUtf8().data(),
+									   hashedPassword.toUtf8().data(),
+									   Utils::jsonToStr(settings).toUtf8().data(),
+									   Utils::jsonToStr(nodeConfig).toUtf8().data(),
+									   Utils::jsonToStr(subAccountData).toUtf8().data());
 
 	return true;
 	// TODO: clear hashedPassword, genAccount
@@ -324,18 +289,16 @@ QString OnboardingModel::importMnemonic(QString mnemonic)
 {
 	// TODO: clear memory
 	QJsonObject obj1{{"mnemonicPhrase", mnemonic}, {"Bip39Passphrase", ""}};
-	const char* importResult = MultiAccountImportMnemonic(
-		Utils::jsonToStr(obj1).toUtf8().data()); // TODO: clear from memory
+
+	const char* importResult = MultiAccountImportMnemonic(Utils::jsonToStr(obj1).toUtf8().data()); // TODO: clear from memory
 	GeneratedAccount acc = jsonObjectToAccount(QJsonDocument::fromJson(importResult).object());
 
+	qCritical() << QJsonDocument::fromJson(importResult).object();
+
 	QJsonObject obj2{{"accountID", acc.id},
-					 {"paths",
-					  QJsonArray{Constants::PathWalletRoot,
-								 Constants::PathEip1581,
-								 Constants::PathWhisper,
-								 Constants::PathDefaultWallet}}};
-	const char* deriveResult = MultiAccountDeriveAddresses(
-		Utils::jsonToStr(obj2).toUtf8().data()); // TODO: clear from memory
+					 {"paths", QJsonArray{Constants::PathWalletRoot, Constants::PathEip1581, Constants::PathWhisper, Constants::PathDefaultWallet}}};
+
+	const char* deriveResult = MultiAccountDeriveAddresses(Utils::jsonToStr(obj2).toUtf8().data()); // TODO: clear from memory
 	setDerivedKeys(acc, QJsonDocument::fromJson(deriveResult).object());
 
 	beginResetModel();
@@ -348,10 +311,7 @@ QString OnboardingModel::importMnemonic(QString mnemonic)
 
 void OnboardingModel::setup(QString accountId, QString password)
 {
-	auto genAccount =
-		std::find_if(mData.begin(), mData.end(), [accountId](const GeneratedAccount& m) -> bool {
-			return m.keyUid == accountId;
-		});
+	auto genAccount = std::find_if(mData.begin(), mData.end(), [accountId](const GeneratedAccount& m) -> bool { return m.keyUid == accountId; });
 	if(genAccount->keyUid == accountId)
 	{
 		QtConcurrent::run([=] {
